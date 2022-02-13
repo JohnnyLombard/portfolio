a6c6317a369b2baa4c0b527161625402821eb9ce
Johnny LOMBARD <johnnylombard@hotmail.fr>
2022-02-13 09:40:04 +0100

    début Javascript

--- quizz/script.js
+++ quizz/script.js
@@ -1,1 +1,11 @@
+class Question {
+  constructor(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+  }
 
+  isCorrectAnswer(choices) {
+    return this.answer === choices;
+  }
+}
